31ccf579db05db147b14acbb90480a385db6a260
shigas <shigasuresh546@gmail.com>
2019-11-23 14:36:01 +0530

    SwiftUI Gradient

--- RadialGradientSwiftUI/ContentView.swift
+++ RadialGradientSwiftUI/ContentView.swift
@@ -8,9 +8,34 @@
 
 import SwiftUI
 
+let gradient = Gradient(colors: [.yellow, .orange, .white])
+let radialGrandient = RadialGradient(gradient: gradient, center: .bottom, startRadius: 100, endRadius: 700)
+let smallRadial = RadialGradient(gradient: gradient, center: .center, startRadius: 1, endRadius: 150)
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello World")
+        ZStack {
+            RoundedRectangle(cornerRadius: 20)
+            .fill(radialGrandient)
+            .edgesIgnoringSafeArea(.vertical)
+            
+            VStack(spacing: 20) {
+                Text("RadialGradient").font(.largeTitle)
+                Text("Introduction").font(.title).foregroundColor(.gray)
+                Text("Radius measure the distance from the center of a circle on outward. You set a startRadius to determine how far the first color should expand outward. The endRadius defines the end of the gradient spread. If the endRadius than thee startRadius, the last defined in the gradient object will fill everything beyond that endRadius.").lineLimit(nil)
+                    
+                Group {
+                    Circle()
+                    .fill(smallRadial)
+                    RoundedRectangle(cornerRadius: 20)
+                    .fill(smallRadial)
+                    .padding(30)
+                    Circle()
+                    .strokeBorder(smallRadial, lineWidth: 40)
+                        .shadow(color: .white, radius: 10)
+                }.shadow(color: .white, radius: 10)
+            }
+        }
     }
 }
 
