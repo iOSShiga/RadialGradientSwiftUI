dddf03799b791e34bdb4aaa58584835a07620f8b
shigas <shigasuresh546@gmail.com>
2019-11-23 12:43:58 +0530

    Initial Commit

---
+++ RadialGradientSwiftUITests/RadialGradientSwiftUITests.swift
@@ -0,0 +1,34 @@
+//
+//  RadialGradientSwiftUITests.swift
+//  RadialGradientSwiftUITests
+//
+//  Created by shiga on 23/11/19.
+//  Copyright © 2019 Shigas. All rights reserved.
+//
+
+import XCTest
+@testable import RadialGradientSwiftUI
+
+class RadialGradientSwiftUITests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
